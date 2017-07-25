743c93289a3b0d44dafb731deac85954d194aa7e
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-07-25 15:59:17 -0500

    starting on Icelandic

---
+++ javascript/icelandic.js
@@ -0,0 +1,9 @@
+var toLanguage = function(toIcelandic){
+
+}
+
+function toIcelandic (){
+
+	let EngtoIcelandic = ["Hamingjusöm", "Frídagar", "Frá", "Okkar", "Lið", "Til", "Þitt"]
+	console.log(EngtoIcelandic);
+}
