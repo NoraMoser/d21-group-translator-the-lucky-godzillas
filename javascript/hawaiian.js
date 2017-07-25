13a28fc3bdfdb7443086ee80f8d11500f1848ef3
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-07-25 15:59:05 -0500

    hawaiian start

---
+++ javascript/hawaiian.js
@@ -0,0 +1,20 @@
+/*
+var toLanguage = (function(toHawaiian) {	
+}
+
+function toHawaiian() {
+	let sentence = {"Happy":"Hau'oli", "Holidays":"icai", "from":"mai", "our":"ka", "team":"hui", "to":"a hiki i", "yours":"kou"};
+};
+
+*/
+
+let EngToHawaiian {
+	"Happy":"Hau'oli",
+	"Holidays":"icai",
+	"from":"mai",
+	"our":"ka",
+	"team":"hui",
+	"to":"a hiki i"
+	"yours":"kou"
+
+};
