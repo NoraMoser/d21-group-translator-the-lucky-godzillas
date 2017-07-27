c7712cf56839fe044292f0857b0a1393d53bb036
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-07-27 08:46:41 -0500

    updated branch

--- javascript/hawaiian.js
+++ javascript/hawaiian.js
@@ -1,20 +1,29 @@
-/*
-var toLanguage = (function(toHawaiian) {	
+var ToLanguage = (function(translator){
+
+let hawaiian = {
+	"HAPPY": "hau'oli",
+	"HOLIDAYS": "icai",
+	"FROM": "mai",
+	"OUR": "ka",
+	"TEAM": "hui",
+	"TO": "a hiki i",
+	"YOUR": "kou",
+	"TEAM": "hui"
 }
+//console.log(hawaiian);
+var translatedArray = [];
 
-function toHawaiian() {
-	let sentence = {"Happy":"Hau'oli", "Holidays":"icai", "from":"mai", "our":"ka", "team":"hui", "to":"a hiki i", "yours":"kou"};
-};
+translator.toHawaiian = function(textArray){
+   //console.log(textArray);
 
-*/
+ textArray.forEach(function(i) {
+      
+      translatedArray += ` ${hawaiian[i]}`;
+    });
+       output.innerHTML = translatedArray;
 
-let EngToHawaiian {
-	"Happy":"Hau'oli",
-	"Holidays":"icai",
-	"from":"mai",
-	"our":"ka",
-	"team":"hui",
-	"to":"a hiki i"
-	"yours":"kou"
+  }
 
-};
+  return translator
+
+})(ToLanguage);
--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,7 @@ translateBtn.addEventListener("click", () => {
   let whichLanguage;
   
   let radios = document.getElementsByClassName("whichLanguage");
-  let textInputHTML = document.getElementById("textInput").innerHTML;
+  let textInputHTML = document.getElementById("textInput").value;
   let textArray = textInputHTML.toUpperCase().split(" ");
  for (let i = 0; i < radios.length; i++) {
     // 1 = French, 2 = Icelandic, 3 = Jamaican, 0 = Hawaiian
@@ -15,16 +15,18 @@ translateBtn.addEventListener("click", () => {
       break;
     }
   }
+
     if(whichLanguage == 0){
       ToLanguage.toHawaiian(textArray);
     } else if (whichLanguage == 1) {
       ToLanguage.toFrench(textArray);
     } else if (whichLanguage == 2) {
       ToLanguage.toIcelandic(textArray);
+
     } else {
       ToLanguage.toJamaican(textArray);
     }
     
-  let output = document.getElementById("output").innerHTML;
+  let output = document.getElementById("output");
 
 });
